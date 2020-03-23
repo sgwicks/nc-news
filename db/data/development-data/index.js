a8a6d64e52f9efd9ff99dee967e4e71b9ac4ad8b
sgwicks <samgwicks@gmail.com>
2020-03-23 12:22:11 +0000

    Added index files for easy data exporting

---
+++ db/data/development-data/index.js
@@ -0,0 +1,6 @@
+const articlesData = require('./articles')
+const commentsData = require('./comments')
+const topicsData = require('./topics')
+const usersData = require('./users')
+
+exports.development = { articlesData, commentsData, topicsData, usersData }
---
+++ db/data/test-data/index.js
@@ -0,0 +1,6 @@
+const articlesData = require('./articles')
+const commentsData = require('./comments')
+const topicsData = require('./topics')
+const usersData = require('./users')
+
+exports.test = { articlesData, commentsData, topicsData, usersData }
