c9aec65605fe67dfac88de4e23e047a66b062980
sgwicks <samgwicks@gmail.com>
2020-03-23 13:45:48 +0000

    Created users table migration

---
+++ db/migrations/20200323133740_create_users_table.js
@@ -0,0 +1,14 @@
+
+exports.up = function (knex) {
+    console.log('Creating users table')
+    return knex.schema.createTable('users', (usersTable) => {
+        usersTable.string('username').primary();
+        usersTable.string('Avatar_url').defaultTo('./img/avatar.jpg');
+        usersTable.string('name').notNullable();
+    })
+};
+
+exports.down = function (knex) {
+    console.log('Dropping users table')
+    return knex.schema.dropTable('users')
+};
