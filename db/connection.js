ff4418eb45d9b0f2d987b57092dc698253779a4b
sgwicks <samgwicks@gmail.com>
2020-03-26 13:57:28 +0000

    Hosting setup

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,9 @@
-const dbConfig = require('../knexfile')
-const connection = require('knex')(dbConfig)
+const ENV = process.env.NODE_ENV || 'development'
+const connection = require('knex')
 
-module.exports = connection
+const dbConfig = 
+    ENV === 'production'
+    ? {client: 'pg', connection: process.env.DATABASE_URL}
+    :require('../knexfile')
+
+module.exports = connection(dbConfig)
--- db/data/index.js
+++ db/data/index.js
@@ -3,6 +3,6 @@ const ENV = process.env.NODE_ENV || 'development'
 const { test } = require('./test-data')
 const { development } = require('./development-data')
 
-const data = { test, development }
+const data = { test, development, production: development }
 
 module.exports = data[ENV]
